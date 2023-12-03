14cded1576ad76473e9312272f6229e9854f5962
Reptile/Jose <130425994+ItzReptile@users.noreply.github.com>
2023-12-03 12:35:26 -0500

    fixed few issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Nav } from "./components/Nav";
+
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
           autoPlay
           loop
           muted
+          playsInline
           className="absolute w-full h-full object-cover z-0"
         >
           <source src="./Tech1.mp4" type="video/mp4" />
@@ -16,12 +17,12 @@ export default function Home() {
         </video>
 
         {/* Centered content over video */}
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10">
-          <div className="p-8 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900 rounded-md shadow-lg flex flex-col items-center justify-center">
+        <div className="absolute  top-0 left-0 w-full h-full flex items-center justify-center z-10">
+          <div className="max-w-[250px] min-[410px]:max-w-[500px]   p-8 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900 rounded-md shadow-lg flex flex-col items-center justify-center">
             <figure className="max-w-xs my-2">
               <img src="./Logo.png" className="w-full" alt="Logo" />
             </figure>
-            <h1 className="text-white text-3xl font-medium">Cole Consulting</h1>
+            <h1 className="text-white text-xl font-medium min-[410px]:text-3xl">Cole Consulting</h1>
             <p className="text-white">Let's Begin Our Journey.</p>
             <div className="flex space-x-4">
               <button
@@ -42,7 +43,7 @@ export default function Home() {
       </div>
 
       {/* Footer */}
-      <footer className="bg-white  shadow p-4 text-center dark:bg-gray-800">
+      <footer className="  shadow p-2 text-center dark:bg-gray-800">
         <span className="text-sm text-gray-500 dark:text-gray-400">
           1300 Eureka St, Lansing, MI 48912
           <span className="text-white"> <a href="mailto:coleconsulting@coleconsulting.io">Email Us!</a> </span>
